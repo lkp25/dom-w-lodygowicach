f798c8a1fe0218da148416e42d56577432317f12
lkp25 <lkp25@o2.pl>
2021-08-19 11:04:15 +0200

    added active tab highlight in js

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,5 +1,16 @@
 const menu = document.querySelector('.menu')
 const links = document.querySelector('.nav-links')
+// highlight active tab:
+const currentUrl = window.location.href
+if(currentUrl.match('kontakt')){
+   links.children[3].children[0].classList.add('nav-link-active')    
+} else if (currentUrl.match('galeria')){
+     links.children[2].children[0].classList.add('nav-link-active')
+} else if (currentUrl.match('lokalizacja')){
+     links.children[1].children[0].classList.add('nav-link-active')
+} else if (currentUrl.match('charakterystyka')){
+     links.children[0].children[0].classList.add('nav-link-active')
+}
 
 menu.addEventListener('click', e=>{
     togglelinks()
