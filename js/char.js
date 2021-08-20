4e1b1295250ee8f5bc8e458e5cbcba8a9a93092a
lkp25 <lkp25@o2.pl>
2021-08-20 18:46:22 +0200

    perfected the animation on wide screen

--- js/char.js
+++ js/char.js
@@ -1,10 +1,16 @@
 const paragraphs = [...document.querySelectorAll('.scroll-animate')]
 
-if(window.innerWidth > 800){
-    paragraphs[0].classList.add('show-text')
-    paragraphs[1].classList.add('show-text')
-    paragraphs[2].classList.add('show-text')
-}
+setTimeout(() => {
+    if(window.innerWidth > 800){
+        paragraphs[0].classList.add('show-text')
+        paragraphs[1].classList.add('show-text')
+        setTimeout(() => {
+            paragraphs[2].classList.add('show-text')
+            
+        }, 250);
+    }
+}, 1000);
+
 
 document.addEventListener('scroll', e=>{
     console.log(window.scrollY);
